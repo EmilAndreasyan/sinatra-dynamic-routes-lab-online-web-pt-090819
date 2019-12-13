314b5d962be3520e44ffb072119138e97460f165
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-12-13 00:23:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,9 +13,10 @@ get '/square/:number' do
 end
 
 get '/say/:number/:phrase' do
-  @num = params[:number]
+  str = ""
   @sentence = params[:phrase]
-  "#{@sentence}" * @num
+  @num = params[:number].to_i
+  
 end
 
 get '/say/:word1/:word2/:word3/:word4/:word5' do
